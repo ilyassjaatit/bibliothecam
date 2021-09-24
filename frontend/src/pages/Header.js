7357e9a1c845ce050dbcd41e76fd06ba2b6b98fd
Ilyass Jaatit <ilyassjaatit@gmail.com>
2021-09-24 11:43:22 +0200

    create layout pages react

---
+++ frontend/src/pages/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Link} from "react-router-dom"
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import Container from 'react-bootstrap/Container';
+
+const Header = () => {
+    return (
+        <React.Fragment>
+            <header>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand as={Link} to="/">{process.env.REACT_APP_NAME}</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll"/>
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="mr-auto my-2 my-lg-0"
+                                style={{maxHeight: '100px'}}
+                                navbarScroll
+                            >
+                                <Nav.Link as={Link} to="/" >Home</Nav.Link>
+
+                            </Nav>
+                            <Form className="d-flex float-end">
+                                <FormControl
+                                    type="search"
+                                    placeholder="Search"
+                                    className="mr-2"
+                                    aria-label="Search"
+                                />
+                                <Button>Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+
+                </Navbar>
+            </header>
+        </React.Fragment>
+    )
+}
+
+export {Header};
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Layout } from './Layout'
+
+const Home = () => {
+    return (
+        <Layout>
+            <div>
+                <h1>This is a home page</h1>
+            </div>
+        </Layout>
+    );
+};
+export default Home;
