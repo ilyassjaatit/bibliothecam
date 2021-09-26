7a6e2248196f0a8258ae517de387c0884b83ce2a
ilyass <ilyassjaatit@gmail.com>
2021-09-26 20:28:57 +0200

    base details book (#10)

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,7 @@
 """api URL Configuration
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,9 +10,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from apps.users.views import UserViewSet
 from apps.libraries.views import BookViewSet
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+                  path("admin/", admin.site.urls),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # api Urls
 router = routers.DefaultRouter()
--- frontend/src/components/book/DetailBook.js
+++ frontend/src/components/book/DetailBook.js
@@ -1,18 +1,21 @@
 import React, {useEffect, useState} from 'react';
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+import Badge from 'react-bootstrap/Badge';
+import Carousel from 'react-bootstrap/Carousel'
+import Dropdown from 'react-bootstrap/Dropdown'
+import Spinner from "react-bootstrap/Spinner";
 
 
 const DetailBook = () => {
-    let { bookId } = useParams();
+    let {bookId} = useParams();
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [book, setBook] = useState([]);
     useEffect(() => {
-        fetch("http://localhost:8000/api/libraries/books/"+bookId+"/")
+        fetch("http://localhost:8000/api/libraries/books/" + bookId + "/")
             .then(res => res.json())
             .then(
                 (result) => {
@@ -27,24 +30,69 @@ const DetailBook = () => {
                     setError(error);
                 }
             )
-    }, [])
-    return (
-        <React.Fragment>
-            <Row>
-                <Col md={4}>
-                <Image src="https://via.placeholder.com/286x180"/>
-                </Col>
-                <Col md={8}>
-                    <div className="box-title">
-                        <h1>{book.name}</h1>
-                        <span className="author">This is author </span>
-                        <div >
-                            book.description
+    }, [bookId])
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <Spinner animation="border"/>
+    } else {
+        return (
+            <React.Fragment>
+                <Row>
+                    <Col md={4}>
+                        <Carousel interval={null}>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src={book.front_cover}
+                                    alt={book.title}
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100"
+                                    src={book.back_cover}
+                                    alt={book.title}
+                                />
+                            </Carousel.Item>
+                        </Carousel>
+                    </Col>
+                    <Col md={8}>
+                        <div className="box-title">
+                            <h1>{book.title}</h1>
+                            <hr/>
+                            <div>
+                                <Badge pill bg="secondary">
+                                    Pages:{book.pages}
+                                </Badge>{' '}
+                                <Badge pill bg="secondary">
+                                    cat: {book.category && book.category.name}
+                                </Badge>{' '}
+                            </div>
+                            <div>
+                                {book.description}
+                            </div>
+
+                            {book.authors && book.authors.map(author => <Badge>{author.name}</Badge>)}
                         </div>
-                    </div>
-                </Col>
-            </Row>
-        </React.Fragment>
-    );
+                        <hr/>
+                        <div>
+                            <Dropdown>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                    Action
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/action-1">Delete</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">Edit</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
+
+                    </Col>
+                </Row>
+            </React.Fragment>
+        );
+    }
 };
 export {DetailBook} ;
