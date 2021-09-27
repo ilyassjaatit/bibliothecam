bc0b092950e5d6c0ca479f50c0c08e280097f7b6
ilyass <ilyassjaatit@gmail.com>
2021-09-28 00:09:19 +0200

    config test and create factories (#14)

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,7 +1,27 @@
 import pytest
 from rest_framework.test import APIClient
+from pytest_factoryboy import register
+
+from tests.factories import UserFactory, BookFactory
+
+# Register factories to pytest global namespace.
+register(UserFactory)
+register(BookFactory)
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
 
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def create_books(db, book_factory):
+    def _create_books(size=10):
+        return book_factory.simple_generate_batch(create=True, size=size)
+
+    return _create_books
