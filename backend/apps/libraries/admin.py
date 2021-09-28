1c9f7422e3d6f8483ab27efb64fe4e302bec0971
Ilyass Jaatit <ilyassjaatit@gmail.com>
2021-09-29 00:02:46 +0200

    backend and nginx config

--- backend/apps/libraries/admin.py
+++ backend/apps/libraries/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 
-from treebeard.admin import TreeAdmin
-from treebeard.forms import movenodeform_factory
 
 from .models import Author, Book, Category
 
 
-class CategoryAdmin(TreeAdmin):
-    form = movenodeform_factory(Category)
-
-
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category)
 admin.site.register(Book)
 admin.site.register(Author)
