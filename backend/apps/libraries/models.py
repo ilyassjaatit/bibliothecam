24e65fd3175b64faa51757bc402645d37e555736
Ilyass Jaatit <ilyassjaatit@gmail.com>
2021-09-29 02:21:14 +0200

    comment add book and add category.field unique

--- backend/apps/libraries/models.py
+++ backend/apps/libraries/models.py
@@ -3,7 +3,7 @@ from django.conf.global_settings import LANGUAGES
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
